510f76628af92b31c31dbc71f28d1357655a5eae
MustafizurRU <mmrsaju.cd@gmail.com>
2024-04-28 23:51:10 +0900

    Last Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -20,16 +21,18 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
-//        if(env('APP_ENV') !== 'local') {
+
+        if(env('REDIRECT_HTTPS'))
+        {
+            $url->forceSchema('https');
+        }
+//        if(env('APP_ENV') === 'production') {
 //            URL::forceScheme('https');
 //        }
-        if(env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
 
 
 
