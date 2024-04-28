f4bb097407849d3c7477917a031766cdbec8b627
MustafizurRU <mmrsaju.cd@gmail.com>
2024-04-28 19:29:19 +0900

    Add Admin panel functionality

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -17,5 +17,5 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
-#echo "Running seeders..."
-#php artisan db:seed --force
+echo "Running seeders..."
+php artisan db:seed --force
